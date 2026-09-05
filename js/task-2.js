a4acea3efcd5c64d54b52f649dea12e2d1f95eae
vladikmukhar-star <vladikmukhar@gmail.com>
2026-09-05 16:10:58 +0200

    Done hw-03

---
+++ js/task-2.js
@@ -0,0 +1,22 @@
+'use strict'
+
+function makeArray(firstArray, secondArray, maxLength) {
+    const allArray = firstArray.concat(secondArray);
+    if (allArray.length > maxLength) {
+        return allArray.slice(0, maxLength)
+    } else {
+        return allArray;
+    }
+}
+
+
+
+
+
+
+//console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+//console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+//console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+//console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+//console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+//console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
---
+++ js/task-3.js
@@ -0,0 +1,19 @@
+'use strict'
+function filterArray(numbers, value) {
+    const numberArray = []
+    for (const number of numbers){
+        if (number > value) {
+    numberArray.push(number)
+}
+    }
+    return numberArray
+}
+
+
+
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
